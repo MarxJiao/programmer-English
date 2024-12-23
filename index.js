01b9ee74c73a6d498b88c68b88518998d3381a90
MarxJiao <marxjiao@gmail.com>
2024-12-23 22:52:08 +0800

    feat: 添加封面

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { getVideo } = require('./lib/main');
 const csvFilePath = process.argv[2];
+const startIndex = +process.argv[3] || 1;
 
-getVideo(csvFilePath);
+getVideo(csvFilePath, startIndex);
